d37739ec73b52cf2881da1d1e1fe538f974c4914
fatteme <fatt3me@gmail.com>
2025-08-26 19:18:41 -0700

    feat(backend): mess up the structure

--- backend/src/routes/media.routes.ts
+++ backend/src/routes/media.routes.ts
@@ -1,17 +1,58 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 import { upload } from '../config/storage';
 import { authenticateToken } from '../middleware/auth.middleware';
-import { MediaController } from '../controllers/media.controller';
+
+import { MediaService } from '../services/media.service';
+import logger from '../utils/logger';
+
+import { UploadImageRequest, UploadImageResponse } from '../types/media.types';
 
 const router = Router();
-const mediaController = new MediaController();
 
 router.post(
   '/upload',
   authenticateToken,
   upload.single('media'),
-  mediaController.uploadImage
+  async (
+    req: Request<{}, {}, UploadImageRequest>,
+    res: Response<UploadImageResponse>,
+    next: NextFunction
+  ) => {
+    try {
+      console.log('req.file', req.file);
+      if (!req.file) {
+        return res.status(400).json({
+          message: 'No file uploaded',
+        });
+      }
+
+      const user = req.user!;
+      const image = await MediaService.saveImage(
+        req.file.path,
+        user._id.toString()
+      );
+
+      console.log('image', image);
+
+      res.status(200).json({
+        message: 'Image uploaded successfully',
+        data: {
+          image,
+        },
+      });
+    } catch (error) {
+      logger.error('Error uploading profile picture:', error);
+
+      if (error instanceof Error) {
+        return res.status(500).json({
+          message: error.message || 'Failed to upload profile picture',
+        });
+      }
+
+      next(error);
+    }
+  }
 );
 
 export default router;
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,29 +1,116 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 
 import { validateBody } from '../middleware/validation.middleware';
 import {
   updateUserHobbiesSchema,
   updateUserProfilePictureSchema,
 } from '../types/user.types';
-import { UserController } from '../controllers/user.controller';
+
+import { userRepository } from '../repositories/user.repository';
+import { MediaService } from '../services/media.service';
+import {
+  GetProfileResponse,
+  UpdateUserHobbiesRequest,
+  UpdateUserProfilePictureRequest,
+} from '../types/user.types';
+import logger from '../utils/logger';
 
 const router = Router();
-const userController = new UserController();
 
-router.get('/profile', userController.getProfile);
+router.get(
+  '/profile',
+  (req: Request<{}, {}, {}>, res: Response<GetProfileResponse>) => {
+    const user = req.user!;
+
+    res.status(200).json({
+      message: 'Profile fetched successfully',
+      data: { user },
+    });
+  }
+);
 
 router.post(
   '/hobbies',
   validateBody(updateUserHobbiesSchema),
-  userController.updateUserHobbies
+  async (
+    req: Request<{}, {}, UpdateUserHobbiesRequest>,
+    res: Response<GetProfileResponse>,
+    next: NextFunction
+  ) => {
+    try {
+      const user = req.user!;
+      const { hobbies } = req.body;
+
+      const updatedUser = await userRepository.update(user._id, { hobbies });
+
+      if (!updatedUser) {
+        return res.status(404).json({
+          message: 'User not found',
+        });
+      }
+
+      res.status(200).json({
+        message: 'User hobbies updated successfully',
+        data: { user: updatedUser },
+      });
+    } catch (error) {
+      logger.error('Failed to update user hobbies:', error);
+
+      if (error instanceof Error) {
+        return res.status(500).json({
+          message: error.message || 'Failed to update user hobbies',
+        });
+      }
+
+      next(error);
+    }
+  }
 );
 
 router.post(
   '/profile-picture',
   validateBody(updateUserProfilePictureSchema),
-  userController.updateUserProfilePicture
-);
+  async (
+    req: Request<{}, {}, UpdateUserProfilePictureRequest>,
+    res: Response<GetProfileResponse>,
+    next: NextFunction
+  ) => {
+    try {
+      console.log('req.body', req.body);
 
-router.delete('/profile-picture', userController.deleteUserProfilePicture);
+      const user = req.user!;
+      const { profilePicture } = req.body;
+
+      if (user.profilePicture) {
+        await MediaService.deleteImage(user.profilePicture);
+      }
+
+      const updatedUser = await userRepository.update(user._id, {
+        profilePicture,
+      });
+
+      if (!updatedUser) {
+        return res.status(404).json({
+          message: 'User not found',
+        });
+      }
+
+      res.status(200).json({
+        message: 'User profile picture updated successfully',
+        data: { user: updatedUser },
+      });
+    } catch (error) {
+      logger.error('Failed to update user profile picture:', error);
+
+      if (error instanceof Error) {
+        return res.status(500).json({
+          message: error.message || 'Failed to update user profile picture',
+        });
+      }
+
+      next(error);
+    }
+  }
+);
 
 export default router;

8a26d51ad956cc85807568b66f713f3b13348b50
fatteme <fatt3me@gmail.com>
2025-08-28 19:18:34 -0700

    fix(frontend): show profile picture updated succesfully after changing the picture

--- frontend/app/src/main/java/com/cpen321/usermanagement/ui/viewmodel/ProfileViewModel.kt
+++ frontend/app/src/main/java/com/cpen321/usermanagement/ui/viewmodel/ProfileViewModel.kt
@@ -110,7 +110,7 @@ class ProfileViewModel(context: Context) : ViewModel() {
         viewModelScope.launch {
             val currentUser = _uiState.value.user ?: return@launch
             val updatedUser = currentUser.copy(profilePicture = imageUri.toString())
-            _uiState.value = _uiState.value.copy(isUploadingProfilePicture = false, user= updatedUser, errorMessage = null)
+            _uiState.value = _uiState.value.copy(isUploadingProfilePicture = false, user= updatedUser, successMessage = "Profile picture updated successfully!")
         }
     }
 
